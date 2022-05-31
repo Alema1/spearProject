787714cb622d3f5eb247cbf85bd35bb1ac951475
Vinicius Schreiner <50752382+Alema1@users.noreply.github.com>
2022-05-31 10:42:04 -0300

    Changes on manual and auto modes

--- spearproject/manualMode.py
+++ spearproject/manualMode.py
@@ -12,21 +12,41 @@ import time
 from msvcrt import kbhit
 import pywinusb.hid as hid
 import serial
+import sys
+
+if sys.version_info >= (3,):
+    # as is, don't handle unicodes
+    unicode = str
+    raw_input = input
+else:
+    # allow to show encoded strings
+    import codecs
 
 joystickID = 0xb108
 
-globals()['XDATA_TO_ARDUINO'] = None
-globals()['XDATA_TO_ARDUINO_ANT'] = None
-globals()['YDATA_TO_ARDUINO'] = None
-globals()['YDATA_TO_ARDUINO_ANT'] = None
-
-
 #ser = serial.Serial('COM3', 19200, serial.EIGHTBITS) #porta do arduino
+
+def Alema1map(valor, in_min, in_max, out_min, out_max):
+    return int((valor-in_min) * (out_max-out_min) / (in_max-in_min) + out_min)
 
 def sample_handler(data):
     # essa funcao tem como objetivo lidar com os dados crus obtidos do joystick
+    # converte o dado do joystick que é recebido em um byte de dado e outro de identificador de quadrante para um numero de 10 bits
+    if data[5] == 0:
+        convertedValue = Alema1map(data[4],0,255,0,255)
+        print(convertedValue)
+    if data[5] == 1:
+        convertedValue = Alema1map(data[4],0,255,255,512)
+        print(convertedValue)
+    if data[5] == 2:
+        convertedValue = Alema1map(data[4],0,255,512,768)
+        print(convertedValue)
+        if data[4] == 0:
+            print('Eixo X centrado')
+    if data[5] == 3:
+        convertedValue = Alema1map(data[4],0,255,768,1023)
+        print(convertedValue)
     
-    print(data)
     '''
     # botoes
     if data[1] == 1:
@@ -70,35 +90,6 @@ def sample_handler(data):
     if data[9] == 128:
         print('Eixo Z centrado')
     '''
-    # TALVEZ POSSA REMOVER
-    global XDATA_TO_ARDUINO # Utilizando a variavel global
-    global YDATA_TO_ARDUINO # Utilizando a variavel global
-    global XDATA_TO_ARDUINO_ANT # Utilizando a variavel global
-    global YDATA_TO_ARDUINO_ANT # Utilizando a variavel global
-    #############
-
-    if(( data [1] > 128) or ( data [1] < 128)):# Eixo X 0-255
-
-        XDATA_TO_ARDUINO = data [1] ## Tentando resolver
-        if (XDATA_TO_ARDUINO != XDATA_TO_ARDUINO_ANT) : XDATA_TO_ARDUINO_ANT = XDATA_TO_ARDUINO # Caso os dados forem diferentes atualiza
-
-    if(( data [2] > 128) or ( data [2] < 128)):# Eixo Y 0-255
-
-        YDATA_TO_ARDUINO = data [2] ## Tentando resolver
-        if (YDATA_TO_ARDUINO != YDATA_TO_ARDUINO_ANT) : YDATA_TO_ARDUINO_ANT = YDATA_TO_ARDUINO # Caso os dados forem diferentes atualiza
-
-    
-    
-        """
-        try:  # Na primeira iteração os valores serao None, por isso o Try Except
-            if(DATA_TO_ARDUINO != DATA_TO_ARDUINO_ANT) : DATA_TO_ARDUINO_ANT = DATA_TO_ARDUINO # Caso os dados forem diferentes atualiza
-        except:
-            pass
-            #DATA_TO_ARDUINO = data[1] # atualiza o valor a ser escrito como o eixo x
-
-        """
-        #ser.write([1,data[1]]) # Removido para a função de loop
-        #print("Eixo X",[1,data[1]]) # Removido para a função de loop
 
 """
     if((data [2]>128)or(data [2]<128)):# Eixo Y 0-255
@@ -133,50 +124,26 @@ def raw_test():
                     print("Joystick Reconhecido!")
                     try:
                         device.open()
-
                         #set custom raw data handler
                         device.set_raw_data_handler(sample_handler)
                         print("\nRecebendo Dados...Pressione qualquer tecla pra sair ")
                         while not kbhit() and device.is_plugged():
-
-                            try: # Nas primeiras iterações a data vai ser None, por isso o try except
-                                print(raw_data)
-                                #print("Eixo X", XDATA_TO_ARDUINO) # Só pra debug
-                                #print("Eixo Y", YDATA_TO_ARDUINO) # Só pra debug
-                                # envia via serial
+                            try: # Nas primeiras iterações a data vai ser None, por isso o try except                    
+                                # tenta enviar via serial
                                 #ser.write([1, XDATA_TO_ARDUINO]) # Data a ser enviada
                                 #ser.write([2, YDATA_TO_ARDUINO]) # Data a ser enviada
-
-                                """
-                                Na versao final o PRINT tem que voltar pra o tratamento dos eixos
-                                """
-
                             except:
-                                #print("Eixo X DEBUGGING ERROR", XDATA_TO_ARDUINO) # Só pra debug
-                                #print("Eixo Y DEBUGGING ERROR", YDATA_TO_ARDUINO) # Só pra debug
-                                pass # Se não conseguir escrever vai para a proxima interaçãoptimize
-
-                            #MANDA DADO A CADA .5 seg
-                            time.sleep(0.04) #RETIRAR VERSÃO FINAL PARA MELHOR DESEMPENHO
-
+                                #print("Erro ao enviar os dados para o microcontrolador!") # Só pra debug
+                                pass # Se não conseguir escrever vai para a proxima interação
+                            #time.sleep(0.04) #retirar para maior desempenho
                         return
                     finally:
                         device.close()
         else:
             print("Joystick não conectado!!")
-#
-if __name__ == '__main__':
-    # first be kind with local encodings
-    import sys
-    if sys.version_info >= (3,):
-        print("a")
-        # as is, don't handle unicodes
-        unicode = str
-        raw_input = input
-    else:
-        print("b")
-        # allow to show encoded strings
-        import codecs
+
         sys.stdout = codecs.getwriter('mbcs')(sys.stdout)
     print("\nReconhecendo Dispositivo e iniciado calibragem... ")
-    raw_test()
+
+# roda 
+raw_test()
