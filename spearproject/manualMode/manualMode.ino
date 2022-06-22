381391972ebae4d83cb4f6d649d8137c37297a68
Vinicius <vinicius.h.s@hotmail.com>
2022-06-22 11:29:29 -0300

    New serial config

--- spearproject/manualMode/manualMode.ino
+++ spearproject/manualMode/manualMode.ino
@@ -6,7 +6,7 @@ Grupo de Automação e Robótica Aplicada/UFSM
 V 1.5
  */
 
-byte incomingByte[3];
+char incomingByte;
 
 // Horizontal 
 
@@ -31,55 +31,48 @@ void setup() {
 }
 
 void passo(int pulPin, int dirPin, int speedM, bool movementDirection){
-  digitalWrite(dirPin, movementDirection);
-  delayMicroseconds(speedM);
-  digitalWrite(pulPin, 1);
-  delayMicroseconds(speedM);
-  digitalWrite(pulPin, 0);
+	digitalWrite(dirPin, movementDirection);
+	delayMicroseconds(speedM);
+	digitalWrite(pulPin, 1);
+	delayMicroseconds(speedM);
+	digitalWrite(pulPin, 0);
 }
 
 void loop(){
-  digitalWrite(enX, 0); 
-  Serial.readBytes(incomingByte,3);
-  // O byte recebido eh assim: [xxx, xxx, xxx]
-  // Primeiro byte eh o cabecalho, segundo eh o quadrante e terceiro valor
-  if(incomingByte[0] == 1){  // Se for eixo X
-    switch (incomingByte[1]){
-      case 0: // Primeiro Quadrante
-        if(incomingByte[2]<128){
-          passo(pulX, dirX, 3000, 1);
-        }
-        else{
-          passo(pulX, dirX, 2000, 1);
-        }
-      break;
-      case 1: //Segundo Quadrante
-        if(incomingByte[2]<128){
-          passo(pulX, dirX, 5000, 1);
-        }
-        else{
-          passo(pulX, dirX, 4000, 1);
-        }
-      break;
-      case 2: // Terceito Quadrante, zero aqui eh o joystick centrado
-        if((incomingByte[2]<128) && (incomingByte[2] != 0)){
-          passo(pulX, dirX, 5000, 0);
-        }
-        if((incomingByte[2]>128) && (incomingByte[2] != 0)){
-          passo(pulX, dirX, 4000, 0);
-        }
-      break;
-      case 3: // Quarto Quadrante
-        if(incomingByte[2]<128){
-          passo(pulX, dirX, 3000, 0);
-        }
-        else{
-          passo(pulX, dirX, 2000, 0);
-        }
-      break;
-      default:
-        //passo(pulX, dirX, 7000, 0);
-      break;
-    }
-  }
+	if(Serial.available() > 0){
+		incomingByte = Serial.read();
+	} 
+  
+	switch(incomingByte){
+		case '0':
+			digitalWrite(enX, 0);
+		break;
+		case '1':
+			passo(pulX, dirX, 5000, 1);
+		break;
+		case '2':
+			passo(pulX, dirX, 4000, 1);
+		break;
+		case '2':
+			passo(pulX, dirX, 3000, 1);
+		break;
+		case '4':
+			passo(pulX, dirX, 2000, 1);
+		break;
+		case '5':
+			passo(pulX, dirX, 5000, 0);
+		break;
+		case '6':
+			passo(pulX, dirX, 4000, 0);
+		break;
+		case '7':
+			passo(pulX, dirX, 3000, 0);
+		break;
+		case '8':
+			passo(pulX, dirX, 2000, 0);
+		break;
+
+	
+	}
+
 }
